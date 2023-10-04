5a514e505728d56fe3de713ec17a99af0add759d
Melissa <138171569+MelissaS27@users.noreply.github.com>
2023-10-04 01:10:09 -0300

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,8 @@
+
+var numero = parseInt(prompt("Digite um numero inteiro:"));
+
+if (numero % 2 === 0 && numero > 0 && numero < 100){
+    alert("O numero atende aos critérios.");
+} else { 
+   alert("O numero não atende aos critérios.");
+ }
